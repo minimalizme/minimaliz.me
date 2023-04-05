79b7e651fc9d0c2024c6170a71105c264df12b30
Hollow <bdelaille@icloud.com>
2023-04-05 18:03:09 +0200

    search and tag

--- search.js
+++ search.js
@@ -1,43 +1,19 @@
-$(document).ready(function () {
-  // Cache les éléments du DOM
-  const $posts = $(".post");
-  const $search = $("#search");
+    document.addEventListener("DOMContentLoaded", function() {
+      const searchInput = document.getElementById("search");
+      const postsContainer = document.getElementById("posts-container");
 
-  // Filtre les posts en fonction du texte de recherche et du tag sélectionné
-  function filterPosts() {
-    const query = $search.val().toLowerCase();
-    const selectedTag = window.location.hash.substring(1);
+      searchInput.addEventListener("input", function(event) {
+        const searchTerm = event.target.value.toLowerCase();
 
-      $posts.each(function () {
-          const $post = $(this);
-          const postContent = $post.text().toLowerCase();
-          //... (Code précédent)
-                const postTags = $post.find("a").map((_, el) => $(el).text()).get();
-                const matchesQuery = !query || postContent.includes(query);
-                const matchesTag = !selectedTag || postTags.includes(selectedTag);
+        Array.from(postsContainer.children).forEach(function(post) {
+          const postTitle = post.querySelector("h2").textContent.toLowerCase();
+          const postContent = post.querySelector("p").textContent.toLowerCase();
 
-                if (matchesQuery && matchesTag) {
-                  $post.show();
-                } else {
-                  $post.hide();
-                }
-              });
-            }
-
-            // Gère l'événement de saisie dans le champ de recherche
-            $search.on("input", function () {
-              window.location.hash = '';
-              filterPosts();
-            });
-
-            // Gère l'événement de clic sur un tag
-            $(".tag-list a").on("click", function (event) {
-              event.preventDefault();
-              const tag = $(this).text();
-              window.location.hash = tag;
-              filterPosts();
-            });
-
-            // Applique le filtrage initial en fonction du hash de l'URL
-            filterPosts();
-          });
+          if (postTitle.includes(searchTerm) || postContent.includes(searchTerm)) {
+            post.style.display = "block";
+          } else {
+            post.style.display = "none";
+          }
+        });
+      });
+    });
